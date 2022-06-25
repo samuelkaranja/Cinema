e12554aaba3d0a7bfc6ec406d5d1703f5da34aaf
samuelkaranja <samuelkaranja007@gmail.com>
2022-06-25 13:44:04 +0300

    File update

--- Cinema/urls.py
+++ Cinema/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+
+from django.views.static import serve
+from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Film.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
---
+++ procFile
@@ -0,0 +1,1 @@
+web: gunicorn Cinema.wsgi --log-file -
